ccadbe57cad2ffda2a4c6f042782e90ab1dd9837
koala1707 <m3tpfbzr@gmail.com>
2022-05-23 12:57:54 +0930

    created client and server

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Main from './pages/Main';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
