b56fa5c668902adf3169cd12621e404faddd3ccc
koala1707 <m3tpfbzr@gmail.com>
2022-06-03 14:00:56 +0930

    formated myself page

--- resume/src/pages/Myself.js
+++ resume/src/pages/Myself.js
@@ -8,6 +8,7 @@ import Sample from '../images/matt.jpg'
 import {HiOutlineMail} from 'react-icons/hi'
 import {AiFillPhone}  from 'react-icons/ai'
 import {GrMapLocation} from 'react-icons/gr'
+import Titlebg from '../images/name-title1.png'
 
 // beginner, intermediate, advanced, expert
 function Myself() {
@@ -19,24 +20,39 @@ function Myself() {
         setInterval(() => setPercentage(0))
     }, [])
     const abilityList = [
-        {percentage: 70, skill: 'JavaScript', bgColor: 'rgb(26, 95, 185)', level: 'Intermediate'},
+        {percentage: 40, skill: 'ASP.NET', bgColor: 'rgb(129, 26, 197)', level: 'Experience'},
+        {percentage: 35, skill: 'C++', bgColor: 'rgb(150, 199, 87)', level: 'Experience'},
+        {percentage: 65, skill: 'CSS', bgColor: 'rgb(24, 158, 91)', level: 'Advanced'},
         {percentage: 50, skill: 'Java', bgColor: 'rgb(26, 95, 185)', level: 'Intermediate'},
-        {percentage: 60, skill: 'React', bgColor: 'rgb(197, 123, 26)', level: 'Intermediate'},
-        {percentage: 55, skill: 'Python', bgColor: 'rgb(26, 134, 197)', level: 'Intermediate'},
-        {percentage: 40, skill: 'ASP.NET', bgColor: 'rgb(129, 26, 197)', level: 'Beginner'},
-        {percentage: 35, skill: 'C++', bgColor: 'rgb(24, 158, 91)', level: 'Beginner'},
+        {percentage: 70, skill: 'JavaScript', bgColor: 'rgb(24, 158, 91)', level: 'Advanced'},
+        {percentage: 55, skill: 'Python', bgColor: 'rgb(196, 183, 2)', level: 'Advanced'},
+        {percentage: 60, skill: 'React', bgColor: 'rgb(24, 158, 91)', level: 'Advanced'},
+        {percentage: 45, skill: 'AWS', bgColor: 'rgb(196, 134, 0)', level: 'Intermediate'},
+        {percentage: 35, skill: 'Docker', bgColor: 'rgb(196, 134, 0)', level: 'Some Experience'},
+        {percentage: 55, skill: 'MySQL', bgColor: 'rgb(196, 134, 0)', level: 'Intermediate'},
+        {percentage: 55, skill: 'SQL Server', bgColor: 'rgb(196, 134, 0)', level: 'Intermediate'},
+        
     ];
     
     return (
         <div className='myself-title'>
+            <div
+                style={{
+                    backgroundImage: `url(${Titlebg})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    height: '20vh',
+                    width: '100vw',
+                }}
+            >
             <h1>Erika Hosokawa</h1>
-            <h5>Software Engineer in the future</h5>
+            <h5 className='sub-title'>Junior Software Engineer</h5>
+            </div>
         <div className="myself-home">
             <div className='left-side'>
                 <img className='photo' src={Sample}/>
                 <h5>CONTACT</h5>
                 <p><HiOutlineMail className='icon-size'/> m3tpfbzr@gmail.com</p>
-                <p><AiFillPhone className='icon-size'/> 0412-938-770</p>
                 <p><GrMapLocation className='icon-size'/> Adelaide, SA Australia</p>
                 
                 <h5>EDUCATION</h5>
@@ -57,23 +73,35 @@ function Myself() {
             </div>
 
             <div className='right-side'>
+                <p className='mysummary'> Experienced an internship in IT industry with collaborating team work and excellent communication skills. Well developed ability to remain calm in stressful situations and mediate positive solutions. Creative and analytical problem solver. Patient, empathic, independent, and willing to learn new skills.
+AWS and Docker experience. Currently studying Bachelor of Information Technology, majoring in Software Development</p>
                 <h3>Work Experience</h3>
                 <div className='work-experience'>
-                    <div className='ai-travel'>
-                    <h4>AI Travel</h4>
-                    <date><GoCalendar className='icon-size'/> July 2022 ~ Current</date>
-                    <h5 className='responsibilities'>Responsibilities</h5>
-                    <div className='res-'>
-                    <p>Assist developers to extract data from database using MySQL</p>
-                    <p>Develop the application on Docker</p>
-                    </div>
-                    <h5 className='achievement'>Achievement</h5>
-                    <p>Created programming to manage programmers’ working hours in google spreadsheet using Google Apps Scripts (GAS)</p>
-                    <p>Created programming to send AWS services cost to Slack and google spreadsheet. Allowing managers to monitor cost movement every six hours with AWS Lambda in Python. (GAS in JavaScript)</p>
-                    <p>Created programming to extract data from database in Ruby with MySQL</p>
-                    <p>Built API using flask and MySQL</p>
-                    </div>
-                    <h4>NTN Corporation</h4>
+                    <h4>AI Travel<h7><GoCalendar className='icon-size'/> July 2022 ~ Current</h7></h4>
+                    
+                    <h5>Responsibilities</h5>
+                    <p>• Assist developers to extract data from database using MySQL</p>
+                    <p>• Develop the application on Docker</p>
+                    <h5>Achievement</h5>
+                    <p>• Created programming to manage programmers’ working hours in google spreadsheet using Google Apps Scripts (GAS)</p>
+                    <p>• Created programming to send AWS services cost to Slack and google spreadsheet. Allowing managers to monitor cost movement every six hours with AWS Lambda in Python. (GAS in JavaScript)</p>
+                    <p>• Created programming to extract data from database in Ruby with MySQL</p>
+                    <p>• Built API using flask and MySQL</p>
+                    <h4>NTN Corporation<h7><GoCalendar className='icon-size'/> October 2014 ~ November 2019</h7></h4>
+                    <h5>Responsibilities</h5>
+                    <p>• Manage a couple of junior staff</p>
+                    <p>• Create document for stakeholders with Excel, Word and PowerPoint</p>
+                    <h5>Achievement</h5>
+                    <p>• Succussed to introduce and install new system to all subsidiaries</p>
+                    <p>• Declared of consumption tax document</p>
+                    <h3>Volunteers</h3>
+                    <p>• University of South Australia Ultimate Treasurer 2020, 2021, 2022</p>
+                    <p>• University Ambassador 2022</p>
+                    <h3>Interest</h3>
+                    <p>• Multi Cultures</p>
+                    <p>• Hiking</p>
+                    <p>• Running</p>
+                    <p>• Traveling</p>
                 </div>
                 
             <div>
