1cc03f4517cce6b887d629b70af1fca015222615
koala1707 <m3tpfbzr@gmail.com>
2022-06-09 14:18:52 +0930

    changed code to launch a website on github

--- resume/src/index.js
+++ resume/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Myself from './pages/Myself'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
