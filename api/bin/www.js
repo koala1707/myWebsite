0c0d66bc6ecdc4f37f54fbb9bad23cbc00fe00d1
koala1707 <m3tpfbzr@gmail.com>
2022-06-11 08:08:34 +0930

    changed port for heroku to be able to assign port number

--- api/bin/www.js
+++ api/bin/www.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 
 // Get port and store in Express.
-var port = ('8090')
+var port = normalisePort(process.env.PORT || '8090');
 app.set('port', port);
 
 // Create HTTP server.
@@ -11,3 +11,17 @@ var server = http.createServer(app);
 
 // Listen on provided port, on all network interfaces
 server.listen(port);
+
+// Normalise a port into number, string or false.
+function normalisePort(val) {
+    var port = parseInt(val, 10);
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+    return false;
+}
