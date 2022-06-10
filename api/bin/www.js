170203c63501e2c234c089c937ccd33196ab71b8
koala1707 <m3tpfbzr@gmail.com>
2022-06-11 08:15:37 +0930

    changed port for heroku to be able to assign port number

--- api/bin/www.js
+++ api/bin/www.js
@@ -3,14 +3,14 @@ var http = require('http');
 
 
 // Get port and store in Express.
-var port = ('8090')
+var port = (process.env.PORT)
 app.set('port', port);
 
 // Create HTTP server.
 var server = http.createServer(app);
 
 // Listen on provided port, on all network interfaces
-// server.listen(port);
-server.listen(process.env.PORT || 8090, function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env)
-})
+server.listen(port);
+// server.listen(process.env.PORT || 8090, function() {
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env)
+// })
