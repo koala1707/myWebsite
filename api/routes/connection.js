f777282a8e2bef68e582319013b248bea10a4b5b
koala1707 <m3tpfbzr@gmail.com>
2022-06-11 14:04:56 +0930

    changed for heroku

--- api/routes/connection.js
+++ api/routes/connection.js
@@ -7,8 +7,8 @@ var knex = require('knex')({
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
-        database: 'PedQLSurveyDatabase',
-        socketPath: '/tmp/mysql.sock'
+        database: 'PedQLSurveyDatabase'
+        // socketPath: '/tmp/mysql.sock'
     }
 });
 
