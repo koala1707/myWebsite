00c8725dab5a8c7e75f6e77d9dbedb71a72cbf86
koala1707 <m3tpfbzr@gmail.com>
2022-06-11 13:22:44 +0930

    changed for heroku

--- api/bin/www.js
+++ api/bin/www.js
@@ -7,8 +7,8 @@ var port = (process.env.PORT || 8090)
 app.set('port', port);
 
 // Create HTTP server.
-//var server = http.createServer(app);
+var server = http.createServer(app);
 
 // Listen on provided port, on all network interfaces
-//server.listen(port);
-app.listen(port)
+server.listen(port);
+// app.listen(port)
