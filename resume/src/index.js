b79e8e5c5412860dbefda153b6061e25ad1e9c26
koala1707 <m3tpfbzr@gmail.com>
2022-06-13 14:08:58 +0930

    changed for heroku

--- resume/src/index.js
+++ resume/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Myself from './pages/Myself'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
